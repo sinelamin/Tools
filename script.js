a5de6cd8a124fd49a9e457d9343c8c357ce650fa
Alexey Mikhaylov <krummi1414@yandex.ru>
2023-10-18 22:11:16 +0300

    feat: add validation for input tel

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const inputNumber = document.querySelector('[name="tel"]');
+
+let phoneNumber = '+7(';
+
+inputNumber.addEventListener('input', (e) => {
+  
+  if (+e.data == e.data && phoneNumber.length < 14) {
+    phoneNumber = `${phoneNumber}${e.data}`;
+    
+    if (phoneNumber.length == 6) {
+      phoneNumber = `${phoneNumber})`;
+    }
+    
+  } else if (e.inputType == 'deleteContentBackward' && phoneNumber.length > 3) {
+    phoneNumber = phoneNumber.slice(0, -1);
+  }
+  
+  inputNumber.value = phoneNumber;
+});
+
+inputNumber.addEventListener('blur', (e) => {
+  if (inputNumber.value.length < 4) {
+    inputNumber.value = '';
+  }
+});
