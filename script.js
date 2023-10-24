e871d1b8e78fa22017d420e4e14d5c4393b2e138
Alexey Mikhaylov <krummi1414@yandex.ru>
2023-10-24 18:56:10 +0300

    feat: add still one form, post form's data to server and fix form's validation

--- script.js
+++ script.js
@@ -2,28 +2,33 @@
 
 import { } from './ajax_get.js';
 
-const inputNumber = document.querySelector('[name="tel"]');
+const inputNumbers = document.querySelectorAll('.form-input__tel');
 
 let phoneNumber = '+7(';
 
-inputNumber.addEventListener('input', (e) => {
+inputNumbers.forEach(input => {
+  input.addEventListener('input', (e) => {
   
-  if (+e.data == e.data && phoneNumber.length < 14) {
-    phoneNumber = `${phoneNumber}${e.data}`;
-    
-    if (phoneNumber.length == 6) {
-      phoneNumber = `${phoneNumber})`;
+    if (+e.data == e.data && phoneNumber.length < 14 && e.data != ' ') {
+      phoneNumber = `${phoneNumber}${e.data}`;
+      
+      if (phoneNumber.length == 6) {
+        phoneNumber = `${phoneNumber})`;
+      }
+      
+    } else if (e.inputType == 'deleteContentBackward' && phoneNumber.length > 3) {
+      phoneNumber = phoneNumber.slice(0, -1);
     }
     
-  } else if (e.inputType == 'deleteContentBackward' && phoneNumber.length > 3) {
-    phoneNumber = phoneNumber.slice(0, -1);
-  }
-  
-  inputNumber.value = phoneNumber;
-});
+    input.value = phoneNumber;
+  });
 
-inputNumber.addEventListener('blur', (e) => {
-  if (inputNumber.value.length < 4) {
-    inputNumber.value = '';
-  }
+  input.addEventListener('focus', () => {
+    if (input.value == '') {
+      inputNumbers.forEach(input => {
+        input.value = '';
+        phoneNumber = '+7(';
+      });
+    }
+  });
 });
