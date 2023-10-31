f11a2fdb0d5c3a07d0b0bf96e0ecb620ccda0c44
Alexey Mikhaylov <krummi1414@yandex.ru>
2023-10-31 22:01:25 +0300

    feat: add adaptive slider

--- slider.js
+++ slider.js
@@ -6,14 +6,23 @@ const cards = document.querySelectorAll('.slider-item');
 const arrowleft = document.querySelector('.slider-btn__left');
 const arrowRight = document.querySelector('.slider-btn__right');
 
-const numberOfCards = 9;
-const marginCard = 50;
+const htmlWidth = document.documentElement.offsetWidth;
 
-// const sliderWrapperWidth = sliderWrapper.offsetWidth;
+const numberOfCards = 9;
+const marginCard = getMarginCard();
+
 const sliderWrapperWidth = sliderWrapper.offsetWidth;
+
+setCardsWidth();
+
 const cardWidth = cards[0].offsetWidth;
 const listWidth = (numberOfCards * (cardWidth + marginCard));
 const step = Math.floor((sliderWrapperWidth / (cardWidth + marginCard)));
+
+console.log('step: ', step);
+console.log('sliderWrapperWidth: ', sliderWrapperWidth);
+console.log('cardWidth + margin: ', cardWidth + marginCard);
+console.log('cardWidth: ', cardWidth);
 
 const offset = (cardWidth + marginCard) * step;
 let position = 0;
@@ -62,3 +71,47 @@ arrowleft.addEventListener('click', () => {
   console.log(position);
   console.log(positionSlide);
 });
+
+function setCardsWidth() {
+
+  cards.forEach(card => {
+    if (htmlWidth > 880) {
+      card.style.minWidth = `${Math.floor((sliderWrapperWidth / 3) - marginCard)}px`;
+    }
+
+    if (htmlWidth > 600 && htmlWidth < 881) {
+      card.style.minWidth = `${Math.floor((sliderWrapperWidth / 2) - marginCard)}px`;
+    }
+
+    if (htmlWidth > 300 && htmlWidth < 601) {
+      card.style.minWidth = `${Math.floor((sliderWrapperWidth / 1) - marginCard)}px`;
+    }
+  });
+}
+
+function getMarginCard() {
+  const marginCss = getComputedStyle(cards[0]).getPropertyValue('margin');
+  const indexSpace = getComputedStyle(cards[0]).getPropertyValue('margin').indexOf(' ');
+  const margin = +marginCss.slice(indexSpace, -2) * 2;
+
+  console.log('card margin: ', margin);
+  return margin;
+}
+
+// function getMarginCard() {
+//   // const marginCss = getComputedStyle(cards[0]).getPropertyValue('margin');
+//   // const indexSpace = getComputedStyle(cards[0]).getPropertyValue('margin').indexOf(' ');
+//   // const margin = +marginCss.slice(indexSpace, -2) * 2;
+
+//   const marginCss = getComputedStyle(cards[0]).getPropertyValue('min-width');
+//   const marginOne = +marginCss.slice(0, -2);
+
+//   const margin = (sliderWrapperWidth - (marginOne * step)) / (step * 2);
+
+//   console.log('card width: ', margin);
+//   return margin;
+// }
+
+// getWidthCard();
+
+console.log(document.documentElement.offsetWidth);
