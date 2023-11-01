a55c37cae00fe75769b8cb02e5b216b5f5deeaa2
Alexey Mikhaylov <krummi1414@yandex.ru>
2023-11-01 19:28:06 +0300

    feat: add generating cards from json, and add class for create cards

--- slider.js
+++ slider.js
@@ -1,79 +1,38 @@
 'use strict';
 
-const sliderWrapper = document.querySelector('.slider-wrapper');
-const sliderList = document.querySelector('.slider-list');
-const cards = document.querySelectorAll('.slider-item');
-const arrowleft = document.querySelector('.slider-btn__left');
-const arrowRight = document.querySelector('.slider-btn__right');
+console.log(document.URL); //*
 
-const htmlWidth = document.documentElement.offsetWidth;
-
-const numberOfCards = 9;
-const marginCard = getMarginCard();
-
-const sliderWrapperWidth = sliderWrapper.offsetWidth;
-
-setCardsWidth();
-
-const cardWidth = cards[0].offsetWidth;
-const listWidth = (numberOfCards * (cardWidth + marginCard));
-const step = Math.floor((sliderWrapperWidth / (cardWidth + marginCard)));
-
-console.log('step: ', step);
-console.log('sliderWrapperWidth: ', sliderWrapperWidth);
-console.log('cardWidth + margin: ', cardWidth + marginCard);
-console.log('cardWidth: ', cardWidth);
-
-const offset = (cardWidth + marginCard) * step;
-let position = 0;
-let positionSlide = 1;
-
-arrowleft.setAttribute('disabled', '');
-arrowleft.style.pointerEvents = 'none';
-
-
-arrowRight.addEventListener('click', () => {
-  if (positionSlide < (numberOfCards / step)) {
-    sliderList.style.transform = `translateX(${position -= offset}px)`;
-
-    arrowleft.removeAttribute('disabled', '');
-    arrowleft.style.pointerEvents = 'auto';
-
-    positionSlide += 1;
-
-    if ((numberOfCards / step) != Math.floor(numberOfCards / step) && positionSlide === Math.ceil((numberOfCards / step))) {
-      sliderList.style.transform = `translateX(${position + (offset / 2)}px)`;
-    }
+// Класс для создания экземпляров карточки
+class Card {
+  constructor(img, title, descr, parent) {
+    this.img = img;
+    this.title = title;
+    this.descr = descr;
+    this.parent = parent;
   }
 
+  render() {
+    const card = document.createElement('div');
 
-  if (positionSlide === Math.ceil((numberOfCards / step))) {
-    arrowRight.setAttribute('disabled', '');
-    arrowRight.style.pointerEvents = 'none';
+    card.classList.add('slider-item');
+    card.innerHTML = `
+          <img class="slider-item__img" src=${this.img} alt="slider-img">
+          <h3 class="slider-item__title">${this.title}</h3>
+          <p class="slider-item__descr">${this.descr}</p>
+      `;
+
+    this.parent.append(card);
   }
-});
+}
+//-------------------------------------------
 
-arrowleft.addEventListener('click', () => {
-  if (positionSlide > 1) {
-    sliderList.style.transform = `translateX(${position += offset}px)`;
+const getCards = async (url) => {
+  const response = await fetch(url);
 
-    arrowRight.removeAttribute('disabled', '');
-    arrowRight.style.pointerEvents = 'auto';
+  return await response.json();
+};
 
-    positionSlide -= 1;
-  }
-
-  if (position === 0) {
-    arrowleft.setAttribute('disabled', '');
-    arrowleft.style.pointerEvents = 'none';
-  }
-
-  console.log(position);
-  console.log(positionSlide);
-});
-
-function setCardsWidth() {
-
+const setCardsWidth = (cards, htmlWidth, sliderWrapperWidth, marginCard) => {
   cards.forEach(card => {
     if (htmlWidth > 880) {
       card.style.minWidth = `${Math.floor((sliderWrapperWidth / 3) - marginCard)}px`;
@@ -87,31 +46,105 @@ function setCardsWidth() {
       card.style.minWidth = `${Math.floor((sliderWrapperWidth / 1) - marginCard)}px`;
     }
   });
-}
+};
 
-function getMarginCard() {
-  const marginCss = getComputedStyle(cards[0]).getPropertyValue('margin');
-  const indexSpace = getComputedStyle(cards[0]).getPropertyValue('margin').indexOf(' ');
+const getMarginCard = (cards) => {
+  const marginCss = getComputedStyle(cards[0]).getPropertyValue('margin'); //*
+  const indexSpace = marginCss.indexOf(' ');
   const margin = +marginCss.slice(indexSpace, -2) * 2;
 
-  console.log('card margin: ', margin);
+  // console.log('card margin: ', margin);
   return margin;
+};
+
+const addSlider = async () => {
+  const sliderWrapper = document.querySelector('.slider-wrapper');
+  const sliderList = document.querySelector('.slider-list');
+  const arrowleft = document.querySelector('.slider-btn__left');
+  const arrowRight = document.querySelector('.slider-btn__right');
+
+  await getCards('http://localhost:3000/cards')
+    .then(data => {
+      data.forEach(({ img, title, descr }) => {
+        new Card(
+          img,
+          title,
+          descr,
+          sliderList
+        ).render();
+      });
+    });
+
+  const cards = document.querySelectorAll('.slider-item');
+
+  console.log(cards);
+
+  const htmlWidth = document.documentElement.offsetWidth; //*
+
+  const numberOfCards = cards.length;
+  const marginCard = getMarginCard(cards);
+
+  const sliderWrapperWidth = sliderWrapper.offsetWidth;
+
+  setCardsWidth(cards, htmlWidth, sliderWrapperWidth, marginCard);
+
+  const cardWidth = cards[0].offsetWidth;
+  const step = Math.floor((sliderWrapperWidth / (cardWidth + marginCard)));
+
+  const offset = (cardWidth + marginCard) * step;
+  let position = 0;
+  let positionSlide = 1;
+
+  arrowleft.setAttribute('disabled', ''); //*
+  arrowleft.style.pointerEvents = 'none';
+
+  arrowRight.addEventListener('click', () => {
+    if (positionSlide < (numberOfCards / step)) {
+      sliderList.style.transform = `translateX(${position -= offset}px)`;
+
+      arrowleft.removeAttribute('disabled', ''); //*
+      arrowleft.style.pointerEvents = 'auto';
+
+      positionSlide += 1;
+
+      if ((numberOfCards / step) != Math.floor(numberOfCards / step) && positionSlide === Math.ceil((numberOfCards / step))) {
+        sliderList.style.transform = `translateX(${position + (offset - (offset * ((numberOfCards / step) - Math.floor(numberOfCards / step))))}px)`;
+      }
+    }
+
+
+    if (positionSlide === Math.ceil((numberOfCards / step))) {
+      arrowRight.setAttribute('disabled', '');
+      arrowRight.style.pointerEvents = 'none';
+    }
+  });
+
+  arrowleft.addEventListener('click', () => {
+    if (positionSlide > 1) {
+      sliderList.style.transform = `translateX(${position += offset}px)`;
+
+      arrowRight.removeAttribute('disabled', '');
+      arrowRight.style.pointerEvents = 'auto';
+
+      positionSlide -= 1;
+    }
+
+    if (position === 0) {
+      arrowleft.setAttribute('disabled', '');
+      arrowleft.style.pointerEvents = 'none';
+    }
+  });
+};
+
+if (`${document.location.pathname}` == '/slider.html') { //*
+  addSlider();
 }
 
-// function getMarginCard() {
-//   // const marginCss = getComputedStyle(cards[0]).getPropertyValue('margin');
-//   // const indexSpace = getComputedStyle(cards[0]).getPropertyValue('margin').indexOf(' ');
-//   // const margin = +marginCss.slice(indexSpace, -2) * 2;
 
-//   const marginCss = getComputedStyle(cards[0]).getPropertyValue('min-width');
-//   const marginOne = +marginCss.slice(0, -2);
 
-//   const margin = (sliderWrapperWidth - (marginOne * step)) / (step * 2);
-
-//   console.log('card width: ', margin);
-//   return margin;
-// }
-
-// getWidthCard();
-
-console.log(document.documentElement.offsetWidth);
+  // new Card(
+  //   './assets/service-card-1.png',
+  //   'title',
+  //   'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+  //   sliderList
+  // ).createCard();
